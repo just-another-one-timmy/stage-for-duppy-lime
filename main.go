6f6a8f3e2ce37ea17d3c336f899714139a032e9d
Iaroslav Tymchenko <jtimchenko@gmail.com>
2014-12-10 21:37:29 +0900

    Basic structure checking, but nothing useful yet

--- main.go
+++ main.go
@@ -7,24 +7,50 @@ import (
 	"path"
 )
 
+func checkStructureAndReturnListOfPosts(basePath string) (files []os.FileInfo, err error) {
+	postsPath := path.Join(basePath, "posts")
+
+	// Note: . and .. are not included.
+	allPostFileInfos, err := ioutil.ReadDir(postsPath)
+	if err != nil {
+		return nil, fmt.Errorf("Can't read directory %s: `%v'\n", postsPath, err)
+	}
+
+	for _, fileInfo := range allPostFileInfos {
+		if fileInfo.IsDir() {
+			return nil, fmt.Errorf("Found dir %s in %s. There must be only regular files.\n", fileInfo.Name(), postsPath)
+		}
+	}
+
+	if len(allPostFileInfos) == 0 {
+		return nil, fmt.Errorf("No posts found!\n")
+	}
+
+	genPath := path.Join(basePath, "gen")
+	err = os.RemoveAll(genPath)
+	// Note: if the path doesn't exist, err will be nil.
+	if err != nil {
+		return nil, fmt.Errorf("Can't remove path %s: `%v'\n", genPath, err)
+	}
+
+	err = os.Mkdir(genPath, 0755)
+	if err != nil {
+		return nil, fmt.Errorf("Can't create path %s: `%v'\n", genPath, err)
+	}
+
+	return allPostFileInfos, nil
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Printf("You must pass a path to the site. No less, no more - just that\n")
 		return
 	}
 
-	basePath := os.Args[1]
-	postsPath := path.Join(basePath, "posts")
-	allPostFileInfos, err := ioutil.ReadDir(postsPath)
+	_, err := checkStructureAndReturnListOfPosts(os.Args[1])
 	if err != nil {
-		fmt.Printf("Can't read directory %s\n", postsPath)
+		fmt.Printf("Failure: %v\n", err)
 		return
 	}
-
-	if len(allPostFileInfos) == 0 {
-		fmt.Printf("No posts found!\n")
-		return
-	}
-
-	fmt.Printf("Didn't encounter any problems, but not going to generate the site either. Come back later when this thing is updated to do somethign useful.\n")
+	fmt.Printf("Didn't encounter any problems, but not going to generate the site either. Come back later when this thing is updated to do something useful.\n")
 }
